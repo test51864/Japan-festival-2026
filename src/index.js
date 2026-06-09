a7fd046319abb4dc38c58f6a5c794e39175c495b
test51864 <ardidelawi01@gmail.com>
2026-06-09 19:37:56 +0200

    Enable random festival field events

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App";
+import "./FestivalEvents.css";
+import "./randomDistractions";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
