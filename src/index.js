aa7291a416df1e1456ba601b17a4bbf534a0787f
test51864 <ardidelawi01@gmail.com>
2026-06-09 19:43:15 +0200

    Load premium start screen styles

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 
 import App from "./App";
 import "./FestivalEvents.css";
+import "./StartScreenPremium.css";
 import "./randomDistractions";
 
 const rootElement = document.getElementById("root");
