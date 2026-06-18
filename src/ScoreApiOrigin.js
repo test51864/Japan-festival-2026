741162e32151c2801898aa654ac498e3b8fe3988
test51864 <ardidelawi01@gmail.com>
2026-06-18 15:19:51 +0200

    Set Vercel API origin for GitHub Pages

---
+++ src/ScoreApiOrigin.js
@@ -0,0 +1,5 @@
+const SCORE_API_ORIGIN = 'https://japan-festival-2026-ardidelawi01-2135s-projects.vercel.app';
+
+if (typeof window !== 'undefined' && !window.YANMAR_SCORE_API_ORIGIN) {
+  window.YANMAR_SCORE_API_ORIGIN = SCORE_API_ORIGIN;
+}
