eec15b98745cdb98620d05725f3ebf913763d927
test51864 <ardidelawi01@gmail.com>
2026-06-18 15:37:07 +0200

    Use multiple score API origins

--- src/ScoreApiOrigin.js
+++ src/ScoreApiOrigin.js
@@ -1,5 +1,9 @@
-const SCORE_API_ORIGIN = 'https://japan-festival-2026-ardidelawi01-2135s-projects.vercel.app';
+const SCORE_API_ORIGINS = [
+  'https://japan-festival-2026-ardidelawi01-2135s-projects.vercel.app',
+  'https://japan-festival-2026.vercel.app',
+];
 
-if (typeof window !== 'undefined' && !window.YANMAR_SCORE_API_ORIGIN) {
-  window.YANMAR_SCORE_API_ORIGIN = SCORE_API_ORIGIN;
+if (typeof window !== 'undefined') {
+  window.YANMAR_SCORE_API_ORIGINS = SCORE_API_ORIGINS;
+  window.YANMAR_SCORE_API_ORIGIN = SCORE_API_ORIGINS[0];
 }
